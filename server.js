036fc68bb7e4c15e1d331fa58e9404e55cbec3f5
kjirou <sorenariblog@gmail.com>
2021-10-26 12:00:37 +0900

    Add routes

--- server.js
+++ server.js
@@ -3,13 +3,34 @@ const express = require('express')
 const port = 3000;
 const app = express()
 
-app.get('/', function (req, res) {
+function createJsScriptTag(options = {}) {
+  const withEval = options.withEval === true ? true : false;
+  return [
+    '<script>',
+    withEval
+      ? 'eval("window.foo = 1 + 2;"); console.log("JS includes `eval`.", foo);'
+      : 'console.log("JS does not include `eval`.");',
+    '</script>',
+  ].join('')
+}
+
+app.get('/without-csp/without-eval', function (req, res) {
   const htmlLines = [
-    '<html>',
-    '<body>',
-    '  <h1>Learn CSP</h1>',
-    '</body>',
-    '</html>',
+    '<html><body>',
+    createJsScriptTag(),
+    '</body></html>',
+  ]
+  res.set({
+    'Content-Type': 'text/html',
+    'Cache-Control': 'no-store',
+  })
+  res.send(htmlLines.join('\n'))
+})
+app.get('/without-csp/with-eval', function (req, res) {
+  const htmlLines = [
+    '<html><body>',
+    createJsScriptTag({withEval: true}),
+    '</body></html>',
   ]
   res.set({
     'Content-Type': 'text/html',
